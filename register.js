469b79ca9740f396828860c825c5876869589121
bo67501 <bo67501@ubt-uni.net>
2023-12-10 01:32:52 +0100

    login-register completed
    login-register completed

---
+++ register.js
@@ -0,0 +1,21 @@
+function validateForm() {
+            var name = document.getElementById("name").value;
+            if (!name.match(/^[a-zA-Z ]+$/)) {
+                document.getElementById("nameError").innerHTML = "Emri duhet të përmbajë vetëm shkronja.";
+                return false;
+            }
+
+            var email = document.getElementById("email").value;
+            if (!email.match(/^([A-Za-z0-9_\-.])+@([A-Za-z0-9_\-.])+\.([A-Za-z]{2,4})$/)) {
+                document.getElementById("emailError").innerHTML = "Adresa email nuk është valide.";
+                return false;
+            }
+
+            var password = document.getElementById("password").value;
+            if (!password.match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/)) {
+                document.getElementById("passwordError").innerHTML = "Fjalëkalimi duhet të përmbajë një shkronjë të madhe dhe një numër.";
+                return false;
+            }
+
+            return true;
+        }
